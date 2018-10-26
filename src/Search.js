a4ac1e6704e94bf8634bf75ad4d5c714ddf25a66
Epris Harris-Burnett <erichardson@iamcreativeteam.com>
2018-10-26 12:48:00 -0400

    Bug: Errors upon clearing query

--- src/Search.js
+++ src/Search.js
@@ -7,64 +7,34 @@ import * as BooksAPI from './BooksAPI'
 
 class Search extends React.Component {
 	state = {
-		books: [],
-		showBooks: [],
+		//books: [],
+		results: [],
 		query: ''
 	}
 
-	componentDidMount() {
-		BooksAPI.getAll().then((books) => {
-			//console.log(books)
-			this.setState({ books }) //Fills array with books
+	componentDidMount(query) {
+		if (query === '' || query === undefined) {
+			this.setState((state) => ({
+				results: []
+			}))
+		}
+		BooksAPI.search(query).then((results) => {
+
 		})
 	}
-
-	updateQuery = (query) => {
-		this.setState({ query: query.trim() }, this.searching)
-		//console.log(this.state.showBooks)
-	}
-
-	clearQuery = (query) => {
-		this.setState({ query: '' })
-	}
-
-	searching() {
-		BooksAPI.search(this.state.query).then((showBooks) => {
-			this.setState({ showBooks })
-			if (showBooks) {
-				const match = new RegExp(escapeRegExp(this.state.query), 'i');
-				this.setState((state) => ({
-					showBooks: this.state.showBooks.filter((book) => match.test(book.title))
-				}))
-			} else {
-				this.setState((state) => ({
-					showBooks: this.state.showBooks
-				}))
-			}
-		})
-	};
 
 	render() {
-		console.log(this.state.showBooks)
+		console.log(this.state.query)
 		return (
 			<div className="search-books">
 				<div className="search-books-bar">
 					<Link to='/' className="close-search">Close</Link>
 					<div className="search-books-input-wrapper">
-						<input
-							type="text"
-							placeholder="Search by title or author"
-							value={this.state.query}
-							onChange={(event) => this.updateQuery(event.target.value)}
-						/>
+						<input type="text" placeholder="Search by title or author"/>
 					</div>
 				</div>
 				<div className="search-books-results">
-					<ol className="books-grid">
-						{this.state.showBooks.map((book, key) => (
-							<Book book={book} key={key}/>
-						))}
-					</ol>
+					<ol className="books-grid"></ol>
 				</div>
 			</div>
 		)
