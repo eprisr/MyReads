f1078ac0ac5d075867d4f5ac232b75be0b9e6277
Epris Harris-Burnett <erichardson@iamcreativeteam.com>
2018-10-26 16:44:11 -0400

    API set up

--- src/App.js
+++ src/App.js
@@ -7,18 +7,37 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    booksOnShelf: [],
     books: []
   }
-
+  componentDidMount() {
+    BooksAPI.getAll().then((booksOnShelf) => {
+      this.setState({ booksOnShelf });
+    })
+  }
+  searchResults(query) {
+    BooksAPI.search(query).then(books => {
+      this.setState(state=> ({
+        books: state.books.concat([ books ])
+      }))
+    })
+  }
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
           <Books
+            booksOnShelf={this.state.booksOnShelf}
+          />
+        )}/>
+        <Route path='/search' render={() => (
+          <Search
+            searchResults={(book) => {
+              this.searchResults(book)
+            }}
             books={this.state.books}
           />
         )}/>
-        <Route path='/search' component={Search} />
       </div>
     )
   }
