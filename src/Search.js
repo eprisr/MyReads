32cc1d0624c5754f30d3cd4e9c1d0b6801b92c05
Epris Harris-Burnett <erichardson@iamcreativeteam.com>
2018-10-26 05:15:01 -0400

    Bug: Searches, Displays, breaks on clearing

--- src/Search.js
+++ src/Search.js
@@ -8,34 +8,44 @@ import * as BooksAPI from './BooksAPI'
 class Search extends React.Component {
 	state = {
 		books: [],
+		showBooks: [],
 		query: ''
 	}
 
 	componentDidMount() {
 		BooksAPI.getAll().then((books) => {
+			//console.log(books)
 			this.setState({ books }) //Fills array with books
 		})
 	}
 
 	updateQuery = (query) => {
-		this.setState({ query: query.trim() })
+		this.setState({ query: query.trim() }, this.searching)
+		//console.log(this.state.showBooks)
 	}
 
 	clearQuery = (query) => {
 		this.setState({ query: '' })
 	}
 
+	searching() {
+		BooksAPI.search(this.state.query).then((showBooks) => {
+			this.setState({ showBooks })
+			if (showBooks) {
+				const match = new RegExp(escapeRegExp(this.state.query), 'i');
+				this.setState((state) => ({
+					showBooks: this.state.showBooks.filter((book) => match.test(book.title))
+				}))
+			} else {
+				this.setState((state) => ({
+					showBooks: this.state.showBooks
+				}))
+			}
+		})
+	};
+
 	render() {
-
-		let showBooks
-		if (this.state.query) {
-			const match = new RegExp(escapeRegExp(this.state.query), 'i');
-			showBooks = this.state.books.filter((book) => match.test(book.title))
-		} else {
-			showBooks = this.state.books;
-		};
-		console.log(showBooks)
-
+		console.log(this.state.showBooks)
 		return (
 			<div className="search-books">
 				<div className="search-books-bar">
@@ -51,7 +61,9 @@ class Search extends React.Component {
 				</div>
 				<div className="search-books-results">
 					<ol className="books-grid">
-
+						{this.state.showBooks.map((book, key) => (
+							<Book book={book} key={key}/>
+						))}
 					</ol>
 				</div>
 			</div>
