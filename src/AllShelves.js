c52c732d30b1858aaa9b1c9197baba5b13d95e95
Epris Harris-Burnett <erichardson@iamcreativeteam.com>
2018-10-25 20:55:30 -0400

    Route home and search

--- src/AllShelves.js
+++ src/AllShelves.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import BookShelf from './BookShelf';
 
@@ -8,37 +9,41 @@ class AllShelves extends React.Component {
 	}
 
 	render() {
-		console.log(this.props.books)
 		return(
-			<div className="list-books">
-				<div className="list-books-title">
-					<h1>MyReads</h1>
-				</div>
-				<div className="list-books-content">
-					<div>
-						<BookShelf
-							name="Currently Reading" //Sets name of bookshelf
-							books={this.props.books.filter((book) => (
-								book.shelf === "currentlyReading" //keeps books with currentlyReading
-							))}
-							updateShelf={this.props.updateShelf} //passes down function
-						/>
-						<BookShelf
-							name="Want to Read"
-							books={this.props.books.filter((book) => (
-								book.shelf === "wantToRead"
-							))}
-							updateShelf={this.props.updateShelf}
-						/>
-						<BookShelf
-							name="Read"
-							books={this.props.books.filter((book) => (
-								book.shelf === "read"
-							))}
-							updateShelf={this.props.updateShelf}
-						/>
+			<div>
+				<div className="list-books">
+					<div className="list-books-title">
+						<h1>MyReads</h1>
+					</div>
+					<div className="list-books-content">
+						<div>
+							<BookShelf
+								name="Currently Reading" //Sets name of bookshelf
+								books={this.props.books.filter((book) => (
+									book.shelf === "currentlyReading" //keeps books with currentlyReading
+								))}
+								updateShelf={this.props.updateShelf} //passes down function
+							/>
+							<BookShelf
+								name="Want to Read"
+								books={this.props.books.filter((book) => (
+									book.shelf === "wantToRead"
+								))}
+								updateShelf={this.props.updateShelf}
+							/>
+							<BookShelf
+								name="Read"
+								books={this.props.books.filter((book) => (
+									book.shelf === "read"
+								))}
+								updateShelf={this.props.updateShelf}
+							/>
+						</div>
 					</div>
 				</div>
+				<Link to='/search' className='open-search'>
+					Add a book
+				</Link>
 			</div>
 		)
 	};
--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI';
+import { Route } from 'react-router-dom';
 import './App.css';
 import AllShelves from './AllShelves';
 import Search from './Search';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: [], //sets empty state array to be filled with API
-    showSearchPage: false
   }
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
@@ -22,32 +16,22 @@ class BooksApp extends React.Component {
   }
   updateShelf = (book, shelf) => { //takes in book and shelf of book clicked
 		BooksAPI.update(book, shelf).then((response) => { //updates
-      console.log(response);
       book.shelf = shelf; //updates book.shelf to shelf
-      console.log(book.shelf);
       this.setState((props) => ({ //changes state, passes function and merges
         books: props.books.filter((b) => b.id !== book.id).concat(book) //filters through books, removes book from on shelf and place on another
       }))
-      console.log(book);
-      console.log(this.state.books.book)
     })
 	}
   render() {
     return (
       <div className='App'>
-        {this.state.showSearchPage ? (
-          <Search />
-        ) : (
+        <Route path='/search' component={Search} />
+        <Route exact path='/' render={() => (
           <AllShelves
             books={this.state.books} //passes down all books
             updateShelf={this.updateShelf} //passes down function
           />
-        )}
-        <div className='open-search'>
-          <a onClick={() => this.setState({ showSearchPage: true })}>
-            Add a book
-          </a>
-        </div>
+        )}/>
       </div>
     )
   };
